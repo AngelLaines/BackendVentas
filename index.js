fd71aff53a4488971571b828c9632a0b0756e501
AngelLaines <angelmoreno755@gmail.com>
2023-10-19 14:45:02 -0700

    post

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const app = express();
 
 app.use(express.json());
 
+
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","*");
-    res.setHeader("Access-Control-Allow-Methods","OPTIONS, GET, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Methods","OPTIONS, GET, POST, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers","Content-Type, Autorization");
     next()
 });
